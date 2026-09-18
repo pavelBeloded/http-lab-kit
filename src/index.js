efe7240f7969bae84534b9b7346bd53c253fd5d1
PavelBeloded <beloded_2007@bk.ru>
2026-09-18 16:27:39 +0300

    fix: export fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { parseUrl, parseParams } from './query.js';
+export { parseUrl, parseParams, readJsonBody } from './query.js';
 export { sendJson, sendText, sendError, serveFile } from './response.js';
 export { createRouter } from './router.js';
 export { createSseHub } from './sse.js';
